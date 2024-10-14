5b00f1f403e49db020b9e55944c445ebffb2347b
una <una_j@naver.com>
2024-10-14 17:54:40 +0900

    ~ 섹션 7. 동기화 - synchronized

---
+++ src/thread/sync/WithdrawTask.java
@@ -0,0 +1,17 @@
+package thread.sync;
+
+public class WithdrawTask implements Runnable {
+
+    private BankAccount bankAccount;
+    private int amount;
+
+    public WithdrawTask(BankAccount bankAccount, int amount) {
+        this.bankAccount = bankAccount;
+        this.amount = amount;
+    }
+
+    @Override
+    public void run() {
+        bankAccount.withdraw(amount);
+    }
+}
